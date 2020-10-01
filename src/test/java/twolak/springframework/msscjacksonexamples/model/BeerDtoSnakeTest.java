8ede29a0da0e03bcc3754a973d0bdd01bd8f66b0
twolak88 <twolak88@gmail.com>
2020-10-01 11:43:35 +0200

    Test snake jackson naming strategy.

---
+++ src/test/java/twolak/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,29 @@
+package twolak.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ *
+ * @author twolak
+ */
+@Slf4j
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeTest extends BaseTest {
+    
+    @Autowired
+    private ObjectMapper objectMapper;
+    
+    @Test
+    public void testSnake() throws JsonProcessingException {
+        BeerDto beerDto = getBeerDto();
+        String jsonString = this.objectMapper.writeValueAsString(beerDto);
+        log.info(jsonString);
+    }
+}
